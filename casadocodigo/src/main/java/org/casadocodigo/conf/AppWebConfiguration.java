695f54ae6ec9fa8b08b044b3ca1440563ff07022
Hebert <hebertcunha1@gmail.com>
2020-02-24 10:05:54 -0300

    Validacao de dados e trabalhando com datas

--- casadocodigo/src/main/java/org/casadocodigo/conf/AppWebConfiguration.java
+++ casadocodigo/src/main/java/org/casadocodigo/conf/AppWebConfiguration.java
@@ -1,9 +1,16 @@
 package org.casadocodigo.conf;
 
+import javax.swing.text.DateFormatter;
+
 import org.casadocodigo.controller.HomeController;
 import org.casadocodigo.daos.ProdutoDAO;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -17,5 +24,24 @@ public class AppWebConfiguration {
 	    resolver.setPrefix("/WEB-INF/views/");
 	    resolver.setSuffix(".jsp");
 	    return resolver;
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("/WEB-INF/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setCacheSeconds(1);
+		
+		return messageSource;
+	}
+	
+	@Bean
+	public FormattingConversionService mvcConversionService() {
+		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+		DateFormatterRegistrar formatterRegistrar = new DateFormatterRegistrar();
+		formatterRegistrar.setFormatter(new org.springframework.format.datetime.DateFormatter("dd/MM/yyyy"));
+		formatterRegistrar.registerFormatters(conversionService);
+		return conversionService;
 	}
 }
--- casadocodigo/src/main/java/org/casadocodigo/controller/ProdutosController.java
+++ casadocodigo/src/main/java/org/casadocodigo/controller/ProdutosController.java
@@ -2,23 +2,37 @@ package org.casadocodigo.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.casadocodigo.daos.ProdutoDAO;
 import org.casadocodigo.model.Produto;
 import org.casadocodigo.model.TipoPrecoEnum;
+import org.casadocodigo.validation.ProdutoValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/produtos")
 public class ProdutosController {
 	
 	@Autowired
 	private ProdutoDAO produtoDao;
 	
-	@RequestMapping("/produtos/form")
-	public ModelAndView form() {
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
+	}
+	
+	@RequestMapping("/form")
+	public ModelAndView form(Produto produto) {
 		
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		System.out.println("Entrando no controller form =D");
@@ -27,19 +41,19 @@ public class ProdutosController {
 		return modelAndView;
 	}
 	
-	@RequestMapping(value="/produtos", method=RequestMethod.POST)
-	public String gravar(Produto produto) {
-
-		System.out.println(produto.getTitulo());
-		System.out.println(produto.getDescricao());
-		System.out.println(produto.getPaginas());
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView gravar(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttibutes) {
+		
+		if(result.hasErrors()) {
+			return form(produto);
+		}
 		
 		produtoDao.gravar(produto);
-		
-		return "produtos/ok";
+		redirectAttibutes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
+		return new ModelAndView("redirect:produtos");
 	}
 	
-	@RequestMapping(value="/produtos", method=RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("produtos/lista");
 		List<Produto> produtos = produtoDao.listar();
